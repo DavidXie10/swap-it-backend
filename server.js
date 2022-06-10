1650f46538c94dfbe0e4ca91a4bf0900b02e0d6f
DavidXie10 <davidxieli@gmail.com>
2022-06-10 15:29:05 -0600

    Added cors

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require('cors');
 
 dotenv.config();
 const server = express();
 server.use(express.json());
+server.use(cors());
 
-server.listen(process.env.PORT || 3001);
-console.log(`The server is listening on http://localhost:${process.env.PORT || 3001}`);
+server.listen(process.env.PORT || 8000);
+console.log(`The server is listening on http://localhost:${process.env.PORT || 8000}`);
