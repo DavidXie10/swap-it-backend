9cf654896c375202b9dea97147018cafc13e2fcd
DavidXie10 <davidxieli@gmail.com>
2022-06-19 00:22:44 -0600

    Added user routes to server

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require('cors');
 
+const userRoutes = require('./routes/users');
+
 dotenv.config();
 const server = express();
 server.use(express.json());
 server.use(cors());
+
+server.use('/users', userRoutes);
 
 server.listen(process.env.PORT || 8000);
 console.log(`The server is listening on http://localhost:${process.env.PORT || 8000}`);
