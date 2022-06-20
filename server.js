c0e6f66ee508c10005bcab53716065e9117d8a35
DavidXie10 <davidxieli@gmail.com>
2022-06-19 21:27:45 -0600

    Adapted server for uploads routes

--- server.js
+++ server.js
@@ -3,9 +3,14 @@ const dotenv = require("dotenv");
 const cors = require('cors');
 
 dotenv.config();
+
+const uploadRoutes = require("./routes/uploads");
+
 const server = express();
 server.use(express.json());
 server.use(cors());
+
+server.use("/uploads", uploadRoutes);
 
 server.listen(process.env.PORT || 8000);
 console.log(`The server is listening on http://localhost:${process.env.PORT || 8000}`);
