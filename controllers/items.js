f81204ddadf6744a5656736b8480433c295939a7
DavidXie10 <davidxieli@gmail.com>
2022-06-25 22:22:16 -0600

    Implemented patch id item endpoint

--- controllers/items.js
+++ controllers/items.js
@@ -9,7 +9,7 @@ exports.createNewItem = (req, res) => {
         const userId = decodedToken.id;
         const user = findUserById(userId);
 
-        res.status(200).json({
+        res.status(201).json({
             message: 'Nuevo item creado exitosamente.',
             item: {
                 idItem: 31,
@@ -20,5 +20,29 @@ exports.createNewItem = (req, res) => {
         })
     } catch (error) {
         res.status(500).json({message: 'Ocurrió un error al crear el artículo. Intente nuevamente. Si el error persiste, contacte al administrador del sistema. Error: ' + error});
+    }
+};
+
+exports.editItem = (req, res) => {
+    try {
+        const userPayload = req.body;
+        const token = req.headers.authorization.split(' ')[1];
+        const decodedToken = jwt.verify(token, process.env.JWT_KEY);
+        const userId = decodedToken.id;
+        const user = findUserById(userId);
+
+        // TODO: change to find item by item id
+        const itemId = req.params.id;
+
+        res.status(200).json({
+            message: 'Item editado exitosamente.',
+            item: {
+                ownerUserId: userId,
+                ownerFullName: user.fullName,
+                ...userPayload,
+            }
+        });
+    } catch (error) {
+        res.status(500).json({message: 'Ocurrió un error al editar el artículo. Intente nuevamente. Si el error persiste, contacte al administrador del sistema. Error: ' + error});
     }
 };
--- routes/items.js
+++ routes/items.js
@@ -5,8 +5,9 @@ const router = express.Router();
 const { checkUserIsAuthenticated } = require('../middlewares/auth');
 const { validateSchema } = require("../middlewares/validation");
 const { itemSchema } = require('../validators/items');
-const { createNewItem } = require('../controllers/items');
+const { createNewItem, editItem } = require('../controllers/items');
 
 router.route('/').post([checkUserIsAuthenticated, validateSchema(itemSchema)], createNewItem);
+router.route('/:id').patch([checkUserIsAuthenticated, validateSchema(itemSchema)], editItem);
 
 module.exports = router;
