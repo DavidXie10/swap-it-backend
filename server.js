df4604e72ede9c2a7831696e5f8bff86a4997dea
DavidXie10 <davidxieli@gmail.com>
2022-06-25 20:55:08 -0600

    Adapted for exchenges endpoint

--- server.js
+++ server.js
@@ -2,18 +2,19 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require('cors');
 
-const userRoutes = require('./routes/users');
-
 dotenv.config();
 
-const uploadRoutes = require("./routes/uploads");
+const userRoutes = require('./routes/users');
+const uploadRoutes = require('./routes/uploads');
+const exchangesRoutes = require('./routes/exchanges');
 
 const server = express();
 server.use(express.json());
 server.use(cors());
 
 server.use('/users', userRoutes);
-server.use("/uploads", uploadRoutes);
+server.use('/uploads', uploadRoutes);
+server.use('/exchanges', exchangesRoutes);
 
 server.listen(process.env.PORT || 8000);
 console.log(`The server is listening on http://localhost:${process.env.PORT || 8000}`);
