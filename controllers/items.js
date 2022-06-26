009e507807dfc061c01fa448ae431b61817591e6
DavidXie10 <davidxieli@gmail.com>
2022-06-25 22:08:46 -0600

    Corrected error 500 message

--- controllers/items.js
+++ controllers/items.js
@@ -19,6 +19,6 @@ exports.createNewItem = (req, res) => {
             }
         })
     } catch (error) {
-        res.status(500).send('Error interno del servidor: ' + error);
+        res.status(500).json({message: 'Ocurrió un error al crear el artículo. Intente nuevamente. Si el error persiste, contacte al administrador del sistema. Error: ' + error});
     }
 };
