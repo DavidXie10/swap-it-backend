8e8b4de164bf94ab2e80180daf9d3f5dbe5af366
DavidXie10 <davidxieli@gmail.com>
2022-06-29 22:19:11 -0600

    Changed previous single and multiple schemas for deleteUrlsSchema for uploads delete endpoint

--- validators/uploads.js
+++ validators/uploads.js
@@ -1,9 +1,5 @@
 const Joi = require('joi');
 
-exports.deleteSingleUrlSchema = Joi.object({
-    urls: Joi.string().uri().required().error(() => new Error('El campo urls es inválido o está vacío. Debe ser una hilera de caracteres.')),
-});
-
-exports.deleteMultipleUrlSchema = Joi.object({
-    urls: Joi.array().min(2).max(5).items(Joi.string().uri().required()).error(() => new Error('El campo urls debe ser un arreglo de hileras de caracteres, con un mínimo de 2 y un máximo de 3, y el formato de las hileras debe ser una url.')),
+exports.deleteUrlsSchema = Joi.object({
+    urls: Joi.array().min(1).max(3).items(Joi.string().uri().required()).error(() => new Error('El campo urls debe ser un arreglo de hileras de caracteres, con un mínimo de 1 y un máximo de 3, y el formato de las hileras debe ser una url.')),
 });
