26e0ab07998df781c743083bcf98aafcc39dd1a5
DavidXie10 <davidxieli@gmail.com>
2022-06-29 22:18:21 -0600

    Made urls field required

--- validators/items.js
+++ validators/items.js
@@ -13,5 +13,5 @@ exports.itemSchema = Joi.object({
     itemState: Joi.number().integer().strict().min(1).max(2).required().error(() => new Error('Debe especificar el estado del item con un número: Nuevo[1], Usado[2].')),
     category: Joi.number().integer().strict().positive().min(1).max(10).required().error(() => new Error('Debe especificar la categoría a la que pertenece el item con un número: Deportes [1], Electrónica[2], Hogar[3], Libros[4], Oficina[5], Películas[6], Ropa[7], Videojuegos[8], Vehículos[9], Otros[10].')),
     location: Joi.number().strict().integer().positive().required().error(() => new Error('Debe especificar la provincia en la que se encuentra con un número: San José[1], Alajuela[2], Cartago[3], Heredia[4], Guanacaste[5], Puntarenas[6], Limón[7].')),
-    photoUrls: Joi.array().min(1).max(3).items(Joi.string().uri().required()).error(() => new Error('El campo photoUrls debe ser un arreglo de hileras de caracteres, con un mínimo de 1 y un máximo de 3, y el formato de las hileras debe ser una url.')),
+    photoUrls: Joi.array().min(1).max(3).required().items(Joi.string().uri().required()).error(() => new Error('El campo photoUrls debe ser un arreglo de hileras de caracteres, con un mínimo de 1 y un máximo de 3, y el formato de las hileras debe ser una url.')),
 });
