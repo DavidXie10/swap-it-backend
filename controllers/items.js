791e49667426743c6a11ee0fd4670f6f81b74366
DavidXie10 <davidxieli@gmail.com>
2022-06-29 22:13:57 -0600

    Added swagger documentation to items endpoints

--- controllers/items.js
+++ controllers/items.js
@@ -1,7 +1,14 @@
 const jwt = require('jsonwebtoken');
-const { items, findItemById, findItemsByCategory, findUserById } = require("../utils/constants");
+const { items, findItemById, findItemsByCategory, findUserById} = require("../utils/constants");
+
+const isItemFromUser = (token, ownerUserId) => {
+    const decodedToken = jwt.verify(token, process.env.JWT_KEY);
+    const userId = decodedToken.id;
+    return ownerUserId !== userId ? false : true;
+}
 
 exports.getAllItems = (req, res) => {
+    // #swagger.tags = ['Items']
     try {
         res.json(items);
     } catch (error) {
@@ -9,7 +16,67 @@ exports.getAllItems = (req, res) => {
     }
 }
 
-exports.createNewItem = (req, res) => {
+exports.createItem = (req, res) => {
+    // #swagger.tags = ['Items']
+    /* 
+    #swagger.description = 'Create a new item with required information'
+    #swagger.parameters['obj'] = {
+            in: 'body',
+            description: 'The new item information',
+            schema: { $ref: '#/definitions/CreateItem' }
+        } 
+    */
+    /* 
+    #swagger.responses[201] = {
+        description: 'Successfully created a new item',
+        schema: {
+            "message": "¡Nuevo item creado exitosamente!",
+            "item": {
+                "itemId": 7,
+                "ownerFullName": "David Xie Li",
+                "ownerUserId": 1,
+                "name": "Cartucho de tinta HP 63",
+                "location": 1,
+                "acquisitionDate": "2021-12-30",
+                "description": "Tengo este cartucho de tinta negra HP 63 que no uso porque cambié de impresora",
+                "wishlist": "Cartucho de tinta negra o de algún color HP 65",
+                "itemState": 1,
+                "category": 2,
+                "photoUrls": ["https://ci0137.s3.amazonaws.com/swap-it/uploads/filename.jpg"]
+            }
+        }
+    } 
+    #swagger.responses[401] = {
+        description: 'Unauthorized. User is not authenticated',
+        schema: {
+            message: 'Credenciales inválidas'
+        }
+    } 
+    #swagger.responses[422] = {
+        description: 'Unprocessable Entity',
+        schema: {
+            "body": {
+                "location": 1,
+                "acquisitionDate": "2021-12-30",
+                "description": "Tengo este cartucho de tinta negra HP 63 que no uso porque cambié de impresora",
+                "wishlist": "Cartucho de tinta negra o de algún color HP 65",
+                "itemState": 1,
+                "category": 2,
+                "photoUrls": [
+                    "https://ci0137.s3.amazonaws.com/swap-it/uploads/filename.jpg"
+                ]
+            },
+            "error": "El nombre del item es un campo obligatorio y debe ser una hilera de caracteres."
+        }
+    }
+    #swagger.responses[500] = {
+        description: 'Internal Server Error',
+        schema: {
+            message: 'Ocurrió un error al crear el artículo. Intente nuevamente. Si el error persiste, contacte al administrador del sistema. Error: el servidor no responde'
+        }
+    } 
+
+    */
     try {
         const userPayload = req.body;
         const token = req.headers.authorization.split(' ')[1];
@@ -18,9 +85,9 @@ exports.createNewItem = (req, res) => {
         const user = findUserById(userId);
 
         res.status(201).json({
-            message: 'Nuevo item creado exitosamente.',
+            message: '¡Nuevo item creado exitosamente!',
             item: {
-                idItem: 31,
+                itemId: 31,
                 ownerUserId: userId,
                 ownerFullName: user.fullName,
                 ...userPayload,
@@ -32,19 +99,21 @@ exports.createNewItem = (req, res) => {
 };
 
 exports.getItem = (req, res) => {
+    // #swagger.tags = ['Items']
     try {
         const item = findItemById(req.params.id);
         if(!item){
-            res.status(404).send('Producto no encontrado');
+            res.status(404).json({message: 'Item no encontrado'});
             return;
         }
-        res.json(item);
+        res.status(200).json(item);
     } catch (error) {
         res.status(500).json({message: 'Ocurrió un error al cargar el artículo. Intente nuevamente. Si el error persiste, contacte al administrador del sistema. Error: ' + error});
     }
 }
 
 exports.getItemsByCategory = (req, res) => {
+    // #swagger.tags = ['Items']
     try {
         const category = req.params.categoryId;
         if(category == 0)
@@ -59,14 +128,88 @@ exports.getItemsByCategory = (req, res) => {
 }
 
 exports.editItem = (req, res) => {
+    // #swagger.tags = ['Items']
+    /*  
+    #swagger.description = 'Edit the information of an existing item only if you own it'    
+    #swagger.parameters['id'] = { description: 'The id of the article for which the information is to be edited' }
+    #swagger.parameters['obj'] = {
+            in: 'body',
+            description: 'The information of the item to be edited',
+            schema: { $ref: '#/definitions/EditItem' }
+        } 
+    */
+    /*
+    #swagger.responses[200] = {
+        description: 'Successfully edited the specified item',
+        schema: {
+            "message": "Item editado exitosamente.",
+            "item": {
+                "ownerUserId": 1,
+                "ownerFullName": "David Xie Li",
+                "name": "Cartucho de tinta HP 63",
+                "location": 1,
+                "acquisitionDate": "2021-12-30",
+                "description": "Este cartucho de tinta negra HP 63 es único en el mundo. No busques más e intercambiemos",
+                "wishlist": "Cartucho de tinta negra o de algún color HP 65",
+                "itemState": 1,
+                "category": 2,
+                "photoUrls": [
+                    "https://ci0137.s3.amazonaws.com/swap-it/uploads/filename.jpg",
+                    "https://ci0137.s3.amazonaws.com/swap-it/uploads/filename2.jpg"
+                ]
+            }
+        }
+    } 
+    #swagger.responses[401] = {
+        description: 'Unauthorized',
+        schema: {
+            message: 'No está autorizado para editar el item.'
+        }
+    } 
+    #swagger.responses[404] = {
+        description: 'The item to be edited does not exist',
+        schema: {
+            message: "El item que se quiere editar no se encuentra"
+        }
+    }
+    #swagger.responses[422] = {
+        description: 'Unprocessable Entity',
+        schema: {
+            "body": {
+                "name": "Cartucho de tinta HP 63",
+                "acquisitionDate": "2021-12-30",
+                "description": "Este cartucho de tinta negra HP 63 es único en el mundo. No busques más e intercambiemos",
+                "wishlist": "Cartucho de tinta negra o de algún color HP 65",
+                "itemState": 1,
+                "category": 2,
+                "photoUrls": [
+                    "https://ci0137.s3.amazonaws.com/swap-it/uploads/filename.jpg",
+                    "https://ci0137.s3.amazonaws.com/swap-it/uploads/filename2.jpg"
+                ]
+            },
+            "error": "Debe especificar la provincia en la que se encuentra con un número: San José[1], Alajuela[2], Cartago[3], Heredia[4], Guanacaste[5], Puntarenas[6], Limón[7]."
+        }
+    }
+    #swagger.responses[500] = {
+        description: 'Internal Server Error',
+        schema: {
+            message: 'Ocurrió un error al editar el artículo. Intente nuevamente. Si el error persiste, contacte al administrador del sistema. Error: el servidor no responde'
+        }
+    } 
+    */
     try {
         const userPayload = req.body;
         const itemId = req.params.id;
         const item = findItemById(itemId);
 
         if(!item){
-            res.status(404).send('Producto no encontrado');
+            res.status(404).json({message: 'El item que se quiere editar no se encuentra'});
             return;
+        }
+
+        if(!isItemFromUser(req.headers.authorization.split(' ')[1], item.ownerUserId)){
+            res.status(401).json({message: 'No está autorizado para editar el item.'});
+            return;      
         }
 
         res.status(200).json({
@@ -83,15 +226,49 @@ exports.editItem = (req, res) => {
 };
 
 exports.deleteItem = (req, res) => {
+    // #swagger.tags = ['Items']
+    /*  
+        #swagger.parameters['id'] = { description: 'The id of the item to be deleted' }
+        #swagger.description = 'Delete an existing item if it exists and if you own the item'
+    */
+    /*
+    #swagger.responses[204] = {
+        description: 'No Content. Successfully deleted the specified item',
+    } 
+    #swagger.responses[401] = {
+        description: 'Unauthorized',
+        schema: {
+            message: 'No está autorizado para eliminar el item.'
+        }
+    } 
+    #swagger.responses[404] = {
+        description: 'The item to be edited does not exist',
+        schema: {
+            message: "El item que se quiere eliminar no se encuentra"
+        }
+    }
+    #swagger.responses[500] = {
+        description: 'Internal Server Error',
+        schema: {
+            message: 'Ocurrió un error al eliminar el artículo. Intente nuevamente. Si el error persiste, contacte al administrador del sistema. Error: el servidor no responde'
+        }
+    } 
+    */
     try {
         const itemId = req.params.id;
-        const exists = findItemById(itemId);
-        
-        if (exists){
-            res.status(204).send();
-        }else{
-            res.status(404).send('El item que se quiere eliminar no se encuentra.');
+        const item = findItemById(itemId);
+    
+        if(!item){
+            res.status(404).json({message: 'El item que se quiere eliminar no se encuentra.'});
+            return;
         }
+    
+        if(!isItemFromUser(req.headers.authorization.split(' ')[1], item.ownerUserId)){
+            res.status(401).json({message: 'No está autorizado para eliminar el item.'});
+            return;      
+        }
+
+        res.status(204).send();
     } catch (error) {
         res.status(500).json({message: 'Ocurrió un error al eliminar el artículo. Intente nuevamente. Si el error persiste, contacte al administrador del sistema. Error: ' + error});
     }
