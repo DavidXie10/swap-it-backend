32ebbf467a92b97a5e0a10b0069c35633fce0cfe
solvalle <vallesol07@gmail.com>
2022-07-02 16:11:54 -0600

    minor fix

--- controllers/users.js
+++ controllers/users.js
@@ -92,6 +92,40 @@ exports.logoutUser = (req, res) => {
 
 exports.updateUser = (req, res) => {
     // #swagger.tags = ['Users']
+    /*  
+    #swagger.description = 'Update user personal information'
+    #swagger.parameters['id'] = { description: 'The id of the user who is updating their data' }
+    #swagger.parameters['obj'] = {
+        in: 'body',
+        description: 'User personal informaction',
+        schema: { $ref: '#/definitions/UpdateUser' }
+    } 
+    */
+    /* #swagger.responses[200] = {
+        description: 'User successfully edited',
+        schema: {
+            "body": {
+                fullName: "John Doe",
+                email: "john@correo.com",
+                location: 1,
+                phoneNumber: "86808521",
+                photoUrl: "http://t1.gstatic.com/licensed-image?q=tbn:ANd9GcTzXP8yS66xjGHKpFXxFA5oSAh0AjA8xPRcU7I88COgApoIfvdk0kenagYFoJhN6u-I",
+            }
+        }
+    } 
+    #swagger.responses[401] = {
+        description: 'Unauthorized. User is not authenticated',
+        schema: {
+            message: 'Credenciales inválidas',
+        }
+    } 
+    #swagger.responses[500] = {
+        description: 'Internal Server Error',
+        schema: {
+            message: 'Ocurrió un error al actualizar la información del usuario. Intente nuevamente. Si el error persiste, contacte al administrador del sistema. Error: el servidor no responde'
+        }
+    } 
+    */
     try {
         // This function is to simulate updating user data in json files
         const updateUserData = (user, userPayload) => true;
@@ -105,14 +139,76 @@ exports.updateUser = (req, res) => {
             res.status(200).json(userPayload);
         }
     } catch(error) {
-        res.status(500).send('Error del servidor ' + error);
+        res.status(500).send('Ocurrió un error al actualizar la información del usuario. Intente nuevamente. Si el error persiste, contacte al administrador del sistema. Error: ' + error);
     }
 }
 
 exports.getItemsByUser = (req, res) => {
     // #swagger.tags = ['Users']
+    /*  
+    #swagger.description = 'Get user items '
+    #swagger.parameters['id'] = { description: 'The id of the user who is requesting their items' }
+    */
+    /* #swagger.responses[200] = {
+        description: 'Successfully user items response',
+        schema: {
+            "body": [
+                {
+                    "itemId": 7,
+                    "ownerFullName": "David Xie Li",
+                    "ownerUserId": 1,
+                    "name": "Cartucho de tinta HP 63",
+                    "location": 1,
+                    "acquisitionDate": "2021-12-30",
+                    "description": "Tengo este cartucho de tinta negra HP 63 que no uso porque cambié de impresora",
+                    "wishlist": "Cartucho de tinta negra o de algún color HP 65",
+                    "itemState": 1,
+                    "category": 2,
+                    "photoUrls": ["https://ci0137.s3.amazonaws.com/swap-it/uploads/filename.jpg"]
+                },
+                {
+                    "itemId": 9,
+                    "ownerFullName": "David Xie Li",
+                    "ownerUserId": 1,
+                    "name": "Cámara web",
+                    "location": 2,
+                    "acquisitionDate": "2021-07-23",
+                    "description": "Cámara web Full HD 1080P con cubierta de privacidad para videollamadas. Video realista de 1920 x 1080p, lente antirreflejo de 4 capas. Distancia focal fija de 1.97 a 197 pulgadas",
+                    "wishlist": "Guantes de box",
+                    "itemState": 2,
+                    "category": 2,
+                    "photoUrls": ["https://ci0137.s3.amazonaws.com/swap-it/uploads/9585387f-cbab-4477-b3ae-4949fe99a1db.jpg"]
+                }
+            ]
+        }
+    } 
+    #swagger.responses[401] = {
+        description: 'Unauthorized. User is not authenticated',
+        schema: {
+            message: 'Credenciales inválidas',
+        }
+    } 
+    #swagger.responses[500] = {
+        description: 'Internal Server Error',
+        schema: {
+            message: 'Ocurrió un error al cargar los artículos del usuario. Intente nuevamente. Si el error persiste, contacte al administrador del sistema. Error: el servidor no responde'
+        }
+    } 
+    */
+
     try {
+        // SHOULD CHECK HERE? IF YES, THE RESPONSE CHANGES (401). MAYBE A MIDDLEWARE?
+        // token = req.headers.authorization.split(' ')[1];
+        // const decodedToken = jwt.verify(token, process.env.JWT_KEY);
+        // if(decodedToken.id != req.params.userId){
+        //     res.status(401).send('No tiene los permisos para acceder a los artículos solicitados');
+        //     return;
+        // }
         const items = findItemsByUser(req.params.userId);
+        if(!items){
+            res.status(404).send('No se encuentra el usuario');
+            return;
+        }
         res.json(items);
     } catch (error) {
         res.status(500).json({message: 'Ocurrió un error al cargar los artículos del usuario. Intente nuevamente. Si el error persiste, contacte al administrador del sistema. Error: ' + error});
--- validators/users.js
+++ validators/users.js
@@ -11,5 +11,5 @@ exports.updateUserSchema = Joi.object({
     email: Joi.string().email().required().error(() => new Error('El email es un campo obligatorio y debe tener el formato de un correo electrónico.')),
     phoneNumber: Joi.string().required().error(() => new Error('El número de teléfono es un campo obligatorio.')),
     location: Joi.number().integer().required().error(() => new Error('La ubicación es un campo obligatorio y debe ser un número entero.')),
-    photoUrl: Joi.string().uri().error(() => new Error('La url de la foto debe tener el formato de un url.'))
+    photoUrl: Joi.string().uri().required().error(() => new Error('La url de la foto es un campo obligatorio y debe tener el formato de un url.'))
 });
