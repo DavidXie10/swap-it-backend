2694a62200ead168672cf3536cd1d4e8149ef7e0
solvalle <vallesol07@gmail.com>
2022-07-04 03:16:46 -0600

    Updateed validations in user schema

--- validators/users.js
+++ validators/users.js
@@ -9,7 +9,7 @@ exports.loginSchema = Joi.object({
 exports.updateUserSchema = Joi.object({
     fullName: Joi.string().required().error(() => new Error('El nombre es un campo obligatorio.')),
     email: Joi.string().email().required().error(() => new Error('El email es un campo obligatorio y debe tener el formato de un correo electrónico.')),
-    phoneNumber: Joi.string().required().error(() => new Error('El número de teléfono es un campo obligatorio.')),
+    phoneNumber: Joi.string().length(8).pattern(/^[0-9]+$/).required().error(() => new Error('El número de teléfono es un campo obligatorio y debe tener el formato de un número de teléfono.')),
     location: Joi.number().integer().required().error(() => new Error('La ubicación es un campo obligatorio y debe ser un número entero.')),
     photoUrl: Joi.string().uri().required().error(() => new Error('La url de la foto es un campo obligatorio y debe tener el formato de un url.'))
 });
